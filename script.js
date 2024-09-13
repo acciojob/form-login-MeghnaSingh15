814c60eef3ab127eb6a3ec5071f8600396e0bb77
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 23:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+let btn = document.querySelector("#button");
+btn.addEventListener("click", function(event) {
+	event.preventDefault();
+	getFormvalue();
+});
+	
 function getFormvalue() {
     //Write your code here
+	let a = document.querySelector("#first_name"); 
+	let b = document.querySelector("#last_name"); 
+	alert(a.value + " " + b.value);
 
 }
